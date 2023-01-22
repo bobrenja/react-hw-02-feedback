2d87db81099220023eec3a5a87ac476e746e66d0
bobrenja <abcd43@ukr.net>
2023-01-22 20:43:40 +0200

    add new project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,57 @@
-import Feedback from "./Feedback/Feedback";
-import css from "./app.module.css"
+import React, { Component } from "react";
 
-export const App = () => {
-  return (
-    <div className={css.project}
-      // style={{
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
-      //   fontSize: 40,
-      //   color: '#010101'
-      // }}
-    >
+export class App extends Component {
+	static defaultProps = {};
+	static propTypes = {};
 
-      <Feedback/>
-    </div>
+	state = {
+		good: 0,
+		neutral: 0,
+		bad: 0,
+	};
 
+	buttonGroup = ["good", "neutral", "bad"];
 
+	changeFeedback = (name) => {
+		this.setState((prevState) => {
+			// console.log(prevState);
+			return {
+				[name]: prevState[name] + 1,
+			};
+		});
+	};
 
+	countTotalFeedback = () => {
+		return Object.values(this.state).reduce(
+			(acc, currentValue) => acc + currentValue,
+			0
+		);
+	};
 
-  );
-};
+	countPositiveFeedbackPercentage = () => {
+		const res = (this.state.good / this.countTotalFeedback()) * 100;
+		return res ? res.toFixed(2) : 0;
+	};
+
+	render() {
+		const { good, neutral, bad } = this.state;
+
+		return (
+			<div>
+				{this.buttonGroup.map((e) => (
+					<button key={e} type="button" onClick={() => this.changeFeedback(e)}>
+						{e}
+					</button>
+				))}
+				Statistics:
+				<ul>
+					<li>good: {good}</li>
+					<li>neutral: {neutral}</li>
+					<li>bad: {bad}</li>
+					<li>Total: {this.countTotalFeedback()}</li>
+					<li>Positiv feedback: {this.countPositiveFeedbackPercentage()}</li>
+				</ul>
+			</div>
+		);
+	}
+}
